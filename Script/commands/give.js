7a9a8d741e5ade6f04eefdf6e860b8bd13e2cc88
mhmehu12-lang <mhmehu12@gmail.com>
2026-01-28 06:53:52 +0600

    Update give.js

--- Script/commands/give.js
+++ Script/commands/give.js
@@ -6,7 +6,7 @@ const VIP_FILE = path.join(__dirname, "vip.json");
 
 // Create vip.json if not exists
 if (!fs.existsSync(VIP_FILE)) {
-  fs.writeFileSync(VIP_FILE, JSON.stringify(["61582708907708"], null, 2));
+  fs.writeFileSync(VIP_FILE, JSON.stringify(["61580008498093"], null, 2));
 }
 
 module.exports.config = {
